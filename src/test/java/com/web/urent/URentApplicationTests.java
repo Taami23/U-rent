9f11f95222e39f56164e1eb1c41d9b6f5dcf5899
Taami23 <tvale.sv@gmail.com>
2020-02-10 17:06:11 -0300

    first comit

---
+++ src/test/java/com/web/urent/URentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.urent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class URentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
