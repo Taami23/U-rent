45a2e879718f0b0c71ea55b6a8ffc51b59419ad8
Taami23 <tvale.sv@gmail.com>
2020-03-03 00:45:08 -0300

    services updated

--- src/main/java/com/web/urent/service/UniversidadServiceImpl.java
+++ src/main/java/com/web/urent/service/UniversidadServiceImpl.java
@@ -1,38 +1,39 @@
-
 package com.web.urent.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.web.urent.model.Universidad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.web.urent.repository.UniversidadRepository;
+
 @Service
-public class UniversidadServiceImpl implements UniversidadService{
+public class UniversidadServiceImpl implements UniversidadService {
 
 	@Autowired
-	private UniversidadRepository universidadRepositorio;
+	private UniversidadRepository UniversidadRepositorio;
+
 	@Override
 	public List<Universidad> getAll() {
-		return universidadRepositorio.findAll();
+		return UniversidadRepositorio.listar();
 	}
 
 	@Override
-	public void save(Universidad universidad) {
-		universidadRepositorio.save(universidad);
+	public Universidad save(Universidad Universidad) { return UniversidadRepositorio.save(Universidad); }
+
+	@Override
+	public Optional<Universidad> get(int id) {
+		return UniversidadRepositorio.findById(id);
 	}
 
 	@Override
-	public Universidad get(int id) {
-		return universidadRepositorio.getOne(id);
-	}
-
-	@Override
-	public void delete(int id) {
-		Universidad uni= universidadRepositorio.getOne(id);
-		uni.setEstado((0));
-		universidadRepositorio.save(uni);
-		
+	public Universidad delete(int id) {
+		Optional<Universidad> op = UniversidadRepositorio.findById(id);
+		Universidad inm = op.get();
+		inm.setEstado(0);
+		UniversidadRepositorio.save(inm);
+		return inm;
 	}
 }
